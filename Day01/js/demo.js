bdf676c5a884a8e2be707b62be2112adbe741761
sara <sara.atef001@gmail.com>
2024-06-03 11:44:22 +0300

    day01

---
+++ Day01/js/demo.js
@@ -0,0 +1,167 @@
+// var fname = "a"; // string
+
+// var num = 10.25858;
+
+// var flag = true;
+
+// /** datatypes in js
+//  *
+//  * primitve datatype
+//  *  string
+//  * number
+//  * boolean
+//  * undefined
+//  * null
+//  *
+//  *
+//  */
+
+// console.log(typeof fname);
+// console.log(typeof num);
+// console.log(typeof flag);
+
+// camel case
+// firstName
+// fristSecond
+// firstSecondThird
+
+// var fname = "Ali"; // inital
+
+// var fname = 10;
+// // fname = false;
+
+// console.log(typeof fname);
+
+// var flag;
+
+// flag = 10;
+
+// console.log(flag);
+// console.log(typeof flag);
+
+// var student = null;
+
+// student = { id: 1, fname: "ali" };
+// console.log(student);
+// console.log(typeof student);
+
+// var fname = 10;
+// console.log(typeof fname);
+// var fname = String(10);
+// console.log(fname);
+// console.log(typeof fname);
+
+// var num = 10;
+// // Nan -- not a number
+// var num = Number("mona");
+
+// console.log(num);
+// console.log(typeof num);
+/** falsy values
+ * false
+ * 0
+ * undefined
+ * nan
+ * null
+ * ""
+ */
+// var flag = false;
+// var flag = Boolean(" sadfsgdhfgj");
+// console.log(flag);
+/** Operation
+ *  [+] --> number + number --> add
+ *          string + string --> concat
+ *
+ *
+ */
+
+// var num1 = "mona";
+
+// var num2 = 20; // "20"
+
+// console.log(num1 + num2);
+
+// console.log(typeof num2);
+
+// var num1 = "mona";
+
+// var num2 = 10;
+
+// console.log(num1 + num2);
+// coercion --> js engine convert from datatype to anther datatype automatically
+/**
+ * comparsion operator
+ * [==] -- only values
+ * [===] --> values & datatype
+ *
+ */
+
+// var num1 = true; // boolean
+
+// var num2 = "true"; // string
+
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+
+/** logical
+ *
+ *  true && true --> true
+ * true && false --> false
+ * false && true --> false
+ * false && false --> false
+ *-----------------------------
+ ||
+
+ true || true --> true
+ true || false --> true 
+ false || true --> true
+ false || false --> false
+
+ --------------------
+ true --> false
+ false --> true
+
+ */
+/**
+ * mona && ali
+ * true &&
+ *
+ * 0 && "pet"
+ * -------------------
+ * "mona" || "ali"
+ *
+ */
+
+// for (var i = 0; i < 5; i++) {
+// 	console.log(i);
+// }
+
+// while(){}
+
+// (condition) ? //line : //line false
+
+// var color = "green";
+
+// color == "red" ? console.log("Red Color") : console.log("No Fav Color");
+/**  */
+
+// var num1 = Number(prompt("Enter Your Number1"));
+// var num2 = Number(prompt("Enter Your Number2"));
+
+// console.log(num1 + num2);
+
+// var fname = prompt("Enter Your Name", "DotNet");
+
+// document.writeln("<h1>" + fname + "</h1>");
+
+// alert("item deleted");
+
+// var result = confirm("Are you sure you want to delete this item");
+// // console.log(result);
+
+// result
+// 	? document.writeln("Item Deleted")
+// 	: document.writeln("Operation Cancelled");
+
+// document.writeln("<h1 class ='test'>Welcome</h1>");
+/** regx in js self study */
